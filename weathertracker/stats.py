e7527eb2aec6be3a8e011ab35a6f789ac98a0579
HackerRank <hackerrank@example.com>
2019-05-19 06:19:54 +0000

    Saved at Sun May 19 2019 02:19:55 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -38,7 +38,7 @@ def validate_stats(stat_names):
 def validate_metric(metric_name, measurement):
     try:
         metric = measurement.get_metric(metric_name):
-    except 
+    except K
         raise InvalidMetricException(metric_name, measurement["timestamp"])
     return measurement[metric]
 
