e282b0da59004df811bd112885fdb34611c0b9f0
HackerRank <hackerrank@example.com>
2019-05-19 05:54:03 +0000

    Saved at Sun May 19 2019 01:54:04 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -3,7 +3,7 @@ from weathertracker.measurement_store import query_measurements
 
 METRICS = {
     "average": lambda metadata: metadata[sum]/metadata[count]
-    "min": lambda metadata: 
+    "min": lambda metadata: metadata
 }
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
