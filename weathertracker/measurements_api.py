afb23fd6c507a149381148d439c729825828f960
HackerRank <hackerrank@example.com>
2019-05-19 04:38:37 +0000

    Saved at Sun May 19 2019 00:38:38 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -57,6 +57,6 @@ class MeasurementsAPI(MethodView):
         try:
             metrics = get_measurement(timestamp)
         except mdb.EntryNotFoundException:
-            raise NotFound
+            raise NotFound()
         # TODO:
         abort(501)
