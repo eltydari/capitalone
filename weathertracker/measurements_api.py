15ee5776b02f67c611dd52b632450eedd685069e
HackerRank <hackerrank@example.com>
2019-05-19 02:58:41 +0000

    Saved at Sat May 18 2019 22:58:41 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -35,10 +35,7 @@ class MeasurementsAPI(MethodView):
     # features/01-measurements/02-get-measurement.feature
     def get(self, timestamp):
 
-        try:
-            timestamp = convert_to_datetime(timestamp)
-        except DatetimeConversionException:
-            raise BadRequest(description = "Timestamp input is invalid: {}".format(timestamp))
+        
 
         # TODO:
         abort(501)
