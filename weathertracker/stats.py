4b9279340f8ab4262c32fa2c2de69709dca4af46
HackerRank <hackerrank@example.com>
2019-05-19 06:34:19 +0000

    Saved at Sun May 19 2019 02:34:20 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -64,7 +64,7 @@ class Stats(object):
                 metadata["max"] = metric
         self._metadata[metric_name] = metadata
         
-    def get
+    def get_stats()
         
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
