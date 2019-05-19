104dfbf9314d410dfa15b47f89a2f08cb28d3a86
HackerRank <hackerrank@example.com>
2019-05-19 06:55:59 +0000

    Saved at Sun May 19 2019 02:56:00 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -54,7 +54,7 @@ class Stats(object):
         for measurement in self.measurements:
             if not validate_metric(metric_name, measurement):
                 return
-            metric = measurement.
+            metric = measurement.get
             metadata["sum"] += metric
             if metadata["min"] is None:
                 metadata["min"] = metric
