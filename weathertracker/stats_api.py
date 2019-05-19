a61c2909f32fd8f63513c17d2db244886401389b
HackerRank <hackerrank@example.com>
2019-05-19 07:19:21 +0000

    Saved at Sun May 19 2019 03:19:21 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats_api.py
+++ weathertracker/stats_api.py
@@ -14,7 +14,7 @@ class StatsAPI(MethodView):
         stats = request.args.getlist("stat")
         print(stats)
         metrics = request.args.getlist("metric")
-        print()
+        print(met)
         from_datetime = request.args.get("fromDateTime")
         to_datetime = request.args.get("toDateTime")
 
