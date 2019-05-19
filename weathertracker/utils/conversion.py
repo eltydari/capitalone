78763278845e1528ca7927fbe9628be8cfc90ac8
HackerRank <hackerrank@example.com>
2019-05-19 01:45:11 +0000

    Saved at Sat May 18 2019 21:45:11 GMT-0400 (Eastern Daylight Time)

--- weathertracker/utils/conversion.py
+++ weathertracker/utils/conversion.py
@@ -12,6 +12,6 @@ def convert_to_datetime(value):
     try:
         value = parse(value)
     except (ValueError, OverflowError):
-        raise DatetimeConversionException("Date input is invalid: {}".)
+        raise DatetimeConversionException("Date input is invalid: {}".format())
     return value
 
