238b0df285ecba860210e9451f76ce65dbc6fd44
HackerRank <hackerrank@example.com>
2019-05-19 04:22:14 +0000

    Saved at Sun May 19 2019 00:22:15 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -19,7 +19,7 @@ def parse_timestamp(timestamp):
     return timestamp
     
 
-def process_metrics(json_metrics):
+def process_metrics():
     try:
         metrics = {k:convert_metric(v) for k,v in req.items() if k != "timestamp"}
     except MetricConversionException as e:
