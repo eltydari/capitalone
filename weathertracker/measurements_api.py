4f00f8c5968b969584aebb0c89e9e055d845c299
HackerRank <hackerrank@example.com>
2019-05-19 04:33:48 +0000

    Saved at Sun May 19 2019 00:33:49 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -27,7 +27,6 @@ def process_measurement(req_json):
             req_json[key] = convert_metric(value)
     except MetricConversionException as e:
         raise BadRequest(description = "Input metric value is not a float: {}".format(e.value))
-    return req_json
 
 
 class MeasurementsAPI(MethodView):
