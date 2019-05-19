e2783fb307c42ab44970d91a93d61e72a9778e7d
HackerRank <hackerrank@example.com>
2019-05-19 03:39:41 +0000

    Saved at Sat May 18 2019 23:39:41 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurement_store.py
+++ weathertracker/measurement_store.py
@@ -17,7 +17,7 @@ def get_measurement(date):
     measurement = dbstore.get(date)
     if not measurement:
         raise EntryNotFoundException
-    return measurement.metrics
+    return measurement
 
 
 def query_measurements(start_date, end_date):
