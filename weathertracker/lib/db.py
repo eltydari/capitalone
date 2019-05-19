ad8dd4ccedf93abf7e3b20512cbe493f5a275344
HackerRank <hackerrank@example.com>
2019-05-19 01:11:54 +0000

    Saved at Sat May 18 2019 21:11:55 GMT-0400 (Eastern Daylight Time)

--- weathertracker/lib/db.py
+++ weathertracker/lib/db.py
@@ -3,7 +3,8 @@ _db_instance = None
 def get_db():
     global _db_instance
     if _db_instance is None:
-        _db_instance = 
+        _db_instance = {}
+        
 
 def teardown_db():
     pass
