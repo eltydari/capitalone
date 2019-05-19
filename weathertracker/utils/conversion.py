be08b796f98a42bf07c672060d04da9ce1b28901
HackerRank <hackerrank@example.com>
2019-05-19 02:13:25 +0000

    Saved at Sat May 18 2019 22:13:26 GMT-0400 (Eastern Daylight Time)

--- weathertracker/utils/conversion.py
+++ weathertracker/utils/conversion.py
@@ -4,7 +4,7 @@ from werkzeug.exceptions import BadRequest
 
 class DatetimeConversionException(BadRequest):
     def __init__(self, message):
-        super.__init__(description = message)
+        super.__init__(description = "message)
 
 
 class MetricConversionException(BadRequest):
