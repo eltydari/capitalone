9202aff4c04533104bef584555c915e3127034c0
HackerRank <hackerrank@example.com>
2019-05-19 04:27:10 +0000

    Saved at Sun May 19 2019 00:27:11 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurement_store.py
+++ weathertracker/measurement_store.py
@@ -18,7 +18,7 @@ def get_measurement(date):
     if not measurement:
         raise EntryNotFoundException
     metrics = measurement.metrics
-    metrics['timestamp']
+    metrics['timestamp'] 
     return measurement.metrics
 
 
