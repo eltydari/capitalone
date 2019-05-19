3af71737362990c2568081132df4b85fa73da89c
HackerRank <hackerrank@example.com>
2019-05-19 02:59:42 +0000

    Saved at Sat May 18 2019 22:59:42 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -16,7 +16,7 @@ def parse_timestamp(timestamp):
         raise BadRequest(description = "Timestamp input is invalid: {}".format(timestamp))
     return timestamp
     
-def expect(f)
+def expect(field)
 
 
 class MeasurementsAPI(MethodView):
