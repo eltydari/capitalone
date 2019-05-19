bca3c2c5a8672f60884a218ba31c3ec1095e88f2
HackerRank <hackerrank@example.com>
2019-05-19 06:17:51 +0000

    Saved at Sun May 19 2019 02:17:52 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -45,7 +45,7 @@ def generate_metadata(metric, measurements):
     metadata = dict(METADATA_TEMPLATE)
     metadata["count"] = len(measurements)
     for measurement in measurements:
-        metadata
+        metadata[]
     
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
