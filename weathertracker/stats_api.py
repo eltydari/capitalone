e88e317b8879ac493cd300ab58a0e4f86e5893a0
HackerRank <hackerrank@example.com>
2019-05-19 08:36:24 +0000

    Saved at Sun May 19 2019 04:36:25 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats_api.py
+++ weathertracker/stats_api.py
@@ -16,7 +16,7 @@ class StatsAPI(MethodView):
             raise BadRequest("No stats were provided.")
         metrics = request.args.getlist("metric")
         if len(metrics) == 0:
-            raise BadRequest("")
+            raise BadRequest("No metri")
         from_datetime = request.args.get("fromDateTime")
         to_datetime = request.args.get("toDateTime")
 
