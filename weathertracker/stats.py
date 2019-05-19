435c6afa0e3778c50a4027f6801d79a0347750f1
HackerRank <hackerrank@example.com>
2019-05-19 05:52:51 +0000

    Saved at Sun May 19 2019 01:52:52 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -2,7 +2,7 @@ from werkzeug.exceptions import abort
 from weathertracker.measurement_store import query_measurements
 
 METRICS = {
-    "m"
+    "min"
 }
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
