9e7b4f17784dcbedea59b59801e12e10d6e0a2e5
HackerRank <hackerrank@example.com>
2019-05-19 03:04:27 +0000

    Saved at Sat May 18 2019 23:04:28 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -29,7 +29,7 @@ class MeasurementsAPI(MethodView):
         
         req = request.get_json()
         
-        timestamp = req.get('')
+        timestamp = req.get('timestamp')
         if not req.get('timestamp')
         
         timestamp = parse_timestamp(req_details[])
