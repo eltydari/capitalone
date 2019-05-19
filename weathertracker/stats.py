2b9e2295cff1287889f3db52601e2f48918c9e06
HackerRank <hackerrank@example.com>
2019-05-19 06:27:39 +0000

    Saved at Sun May 19 2019 02:27:40 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -31,7 +31,7 @@ class InvalidMetricException(Exception):
 class Stats(object):
     def __init__(self, start_date, end_date):
         self.measurements = query_measurements(from_datetime, to_datetime)
-        self._metadata = _generate_metadata
+        self._metadata = _generate_metadata()
         
     def 
 
