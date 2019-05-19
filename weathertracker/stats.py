27a8ca6b76bef433d31d85aea06b07a200a38092
HackerRank <hackerrank@example.com>
2019-05-19 06:41:01 +0000

    Saved at Sun May 19 2019 02:41:02 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -76,4 +76,5 @@ class Stats(object):
 def get_stats(stats, metrics, from_datetime, to_datetime):
     validate_stats(stats)
     stats_obj = Stats(metrics, from_datetime, to_datetime)
-    for stat_name in stats
+    for stat_name in stats:
+        
