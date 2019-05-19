f338977fd9bb78cdd93426726f5c325987b27135
HackerRank <hackerrank@example.com>
2019-05-19 06:05:30 +0000

    Saved at Sun May 19 2019 02:05:31 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -25,7 +25,7 @@ class InvalidStatException(Exception):
 def validate_stats(stat_names):
     for name in stat_names:
         if name not in stats:
-            raise InvalidStatExcepti
+            raise InvalidStatException()
 
 
 def generate_metadata(measurements):
