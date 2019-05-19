29048e4eb4869e62742a97158c51291c2e8c8b6c
HackerRank <hackerrank@example.com>
2019-05-19 07:00:13 +0000

    Saved at Sun May 19 2019 03:00:14 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -27,6 +27,7 @@ def validate_stats(stat_names):
 def validate_metric(metric_name, measurement):
     return bool(measurement.get_metric(metric_name))
 
+
 def validate_metadata(data):
     
         
