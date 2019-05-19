a504af50a946b74ec335c49993739bc82dad6159
HackerRank <hackerrank@example.com>
2019-05-19 01:40:05 +0000

    Saved at Sat May 18 2019 21:40:05 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -20,7 +20,7 @@ class MeasurementsAPI(MethodView):
         try:
             timestamp = convert_to_datetime(timestamp)
         except DatetimeConversionException:
-            raise BadRequest(d)
+            raise BadRequest(description =)
 
         # TODO:
         abort(501)
