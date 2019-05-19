1d5e019c46fb60c4aeaba1f3853a0695b455023e
HackerRank <hackerrank@example.com>
2019-05-19 03:55:11 +0000

    Saved at Sat May 18 2019 23:55:12 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -34,7 +34,7 @@ class MeasurementsAPI(MethodView):
         metrics = {k:convert_metric(v) for k,v in req.items() if k != "timestamp"}
         mdb.add_measurement(timestamp, metrics)
         
-        return Response("Success!")
+        return Response("Success!",)
 
     # features/01-measurements/02-get-measurement.feature
     def get(self, timestamp):
