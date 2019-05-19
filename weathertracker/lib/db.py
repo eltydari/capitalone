86bfe6fc817fb686b65fbe6aa13bf639ab0e9213
HackerRank <hackerrank@example.com>
2019-05-19 01:17:39 +0000

    Saved at Sat May 18 2019 21:17:39 GMT-0400 (Eastern Daylight Time)

--- weathertracker/lib/db.py
+++ weathertracker/lib/db.py
@@ -1,7 +1,7 @@
 """
 Simple dictionary db interface that lives in process memory.
 
-Can be extended in the future to accept more types of dbs
+Can be extended in the future to accept more types of dbs.
 """
 
 _db_instance = None
