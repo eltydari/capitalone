c0158d6712e9ef3779a0850669a871d7fac4e12a
HackerRank <hackerrank@example.com>
2019-05-19 08:28:04 +0000

    Saved at Sun May 19 2019 04:28:05 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -4,7 +4,7 @@ from weathertracker.measurement_store import query_measurements
 
 # Registered metrics will use metadata generated within get_stats
 STATS = {
-    # NOTE: We are rounding division to 10 digits to 
+    # NOTE: We are rounding division to 10 digits to mitig
     "average": lambda metadata: round(metadata["sum"]/metadata["count"],10),
     "min": lambda metadata: metadata["min"],
     "max": lambda metadata: metadata["max"]
