7287c3603456233aa7d22025057db83dfd6a3999
HackerRank <hackerrank@example.com>
2019-05-19 10:39:52 +0000

    Saved at Sun May 19 2019 06:39:53 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -39,7 +39,7 @@ def validate_metadata(data):
     
         
 class Stats(object):
-    #The point of this object is to save compute time by aggre
+    #The point of this object is to save compute time by aggregating
     def __init__(self, metrics, start_date, end_date):
         self.measurements = query_measurements(start_date, end_date)
         self._aggregate_metrics(metrics)
