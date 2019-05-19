5c18daf398981b67d777904d24ef67844f06d5ea
HackerRank <hackerrank@example.com>
2019-05-19 05:54:52 +0000

    Saved at Sun May 19 2019 01:54:53 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -2,11 +2,10 @@ from werkzeug.exceptions import abort
 from weathertracker.measurement_store import query_measurements
 
 METRICS = {
-    "average": lambda data: metadata[sum]/metadata[count],
+    "average": lambda metadata: metadata[sum]/metadata[count],
     "min": lambda metadata: metadata[min],
     "max": lambda metadata: metadata[max]
 }
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
-   # TODO: 
-   abort(501)
+   
