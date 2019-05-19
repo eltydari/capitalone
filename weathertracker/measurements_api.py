77395136e0981a4f29c6df242bfc2c4744e1de2e
HackerRank <hackerrank@example.com>
2019-05-19 09:37:36 +0000

    Saved at Sun May 19 2019 05:37:37 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -25,7 +25,7 @@ def process_measurement(pairs):
             if key == "timestamp":
                 continue
             if not isinstance(key, str):
-                raise BadRequest()
+                raise BadRequest(descript)
             pairs[key] = convert_metric(value)
     except MetricConversionException as e:
         raise BadRequest(description = "Input metric value is not a float: {}".format(e.value))
