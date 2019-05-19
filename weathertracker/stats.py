0e880c959382d76e40127a32b5004484ccdc8d0c
HackerRank <hackerrank@example.com>
2019-05-19 06:24:02 +0000

    Saved at Sun May 19 2019 02:24:02 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -55,7 +55,7 @@ def generate_metadata(metric_name, measurements):
             metadata["min"] = metric
         if metadata["max"] is None:
             metadata["max"] = metrics
-        elif metric > metadata
+        elif metric > metadata["max"]
     
 
 def get_stats(stats, metrics, from_datetime, to_datetime):
