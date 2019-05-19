1cc2e6919158fc71dc70af8fae4b0aec974e88f9
HackerRank <hackerrank@example.com>
2019-05-19 03:07:21 +0000

    Saved at Sat May 18 2019 23:07:22 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -29,7 +29,8 @@ class MeasurementsAPI(MethodView):
             raise BadRequest(description = "Timestamp was expected in the post request but not provided")
         timestamp = parse_timestamp(timestamp)
         
-        for key, value in r
+        for key, value in req:
+            
         
         return jsonify(message="Success!")
 
