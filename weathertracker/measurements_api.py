cba314b3162b7b45483a34adc01ded84b888818a
HackerRank <hackerrank@example.com>
2019-05-19 04:09:50 +0000

    Saved at Sun May 19 2019 00:09:51 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -34,7 +34,7 @@ class MeasurementsAPI(MethodView):
         try:
             metrics = {k:convert_metric(v) for k,v in req.items() if k != "timestamp"}
         except MetricConversionException:
-            raise BadRequest(description = "Metric is not a float: \"{}\" with value {}'".format(k, v))
+            raise BadRequest(description = "Metric is not a float: \"{}\" with value \"{}'".format(k, v))
             
         mdb.add_measurement(timestamp, metrics)
         
