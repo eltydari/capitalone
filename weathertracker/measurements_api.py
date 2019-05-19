a53974668257ceff4d74684b5e46f30cc90fb29f
HackerRank <hackerrank@example.com>
2019-05-19 03:05:28 +0000

    Saved at Sat May 18 2019 23:05:29 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -31,7 +31,7 @@ class MeasurementsAPI(MethodView):
         
         timestamp = req.get('timestamp')
         if not timestamp:
-            raise BadRequest(description = "Timestamp was exp")
+            raise BadRequest(description = "Timestamp was expected in the ")
         
         timestamp = parse_timestamp(req_details[])
         
