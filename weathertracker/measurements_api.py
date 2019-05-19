fe2029584d99a6f54fd5fcb261ce95e1a8b8625c
HackerRank <hackerrank@example.com>
2019-05-19 04:26:27 +0000

    Saved at Sun May 19 2019 00:26:28 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -51,6 +51,6 @@ class MeasurementsAPI(MethodView):
 
         timestamp = parse_timestamp(timestamp)
 
-        get_measurement(timestamp)
+        mget_measurement(timestamp)
         # TODO:
         abort(501)
