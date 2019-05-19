798bbf37aa1a024f86f24014f180ce994c59fe33
HackerRank <hackerrank@example.com>
2019-05-19 03:08:24 +0000

    Saved at Sat May 18 2019 23:08:25 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -32,6 +32,7 @@ class MeasurementsAPI(MethodView):
         for key, value in req.iteritems():
             if key == 'timestamp':
                 timestamp = req.get('timestamp')
+
         
         return jsonify(message="Success!")
 
