8c2742a38f688ac5d977898e6ab725f900b087d7
HackerRank <hackerrank@example.com>
2019-05-19 04:36:33 +0000

    Saved at Sun May 19 2019 00:36:34 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurement_store.py
+++ weathertracker/measurement_store.py
@@ -10,7 +10,7 @@ class EntryNotFoundException(Exception):
 def add_measurement(measurement):
     dbstore = db.get_db()
     date = measurement["timestamp"]
-    metrics = {k:v for k,v in measurment}
+    metrics = {k:v for k,v in measurment.}
     dbstore[date] = Measurement(date, metrics)
 
 
