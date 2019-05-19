c3e936c9eda58db3ce44d420e77a01d34202e1b0
HackerRank <hackerrank@example.com>
2019-05-19 04:50:11 +0000

    Saved at Sun May 19 2019 00:50:12 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -46,7 +46,7 @@ class MeasurementsAPI(MethodView):
         mdb.add_measurement(req)
         
         resp = Response("Success!", status=201)
-        print(timep)
+        print(timeste)
         resp.headers.add("Location", "/measurements/{}".format(timestamp))
         return resp
 
