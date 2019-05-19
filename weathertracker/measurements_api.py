990eaf71ab81ef155eccd2aba6cd5613c9c45fd5
HackerRank <hackerrank@example.com>
2019-05-19 02:38:25 +0000

    Saved at Sat May 18 2019 22:38:25 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -15,7 +15,7 @@ class MeasurementsAPI(MethodView):
     def post(self):
         # TODO:
         print(request.args)
-        return({'message':''})
+        return({'message':'Success'})
 
     # features/01-measurements/02-get-measurement.feature
     def get(self, timestamp):
