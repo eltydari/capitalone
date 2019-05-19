1df28d28934fb9c7b9641ae2767c180f1315f0b4
HackerRank <hackerrank@example.com>
2019-05-19 05:10:12 +0000

    Saved at Sun May 19 2019 01:10:13 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -68,4 +68,4 @@ class MeasurementsAPI(MethodView):
         except mdb.EntryNotFoundException:
             raise NotFound("Entry was not found in database.")
             
-        return jsonify(stringify(measurement)
+        return jsonify(stringify(measurement))
