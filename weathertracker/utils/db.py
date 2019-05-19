bcdabde12f86229b43d39bd297f58c326a5fefb6
HackerRank <hackerrank@example.com>
2019-05-19 02:06:03 +0000

    Saved at Sat May 18 2019 22:06:03 GMT-0400 (Eastern Daylight Time)

--- weathertracker/utils/db.py
+++ weathertracker/utils/db.py
@@ -3,7 +3,7 @@ Simple dictionary db interface that lives in process memory.
 
 Can be extended in the future to accept more types of dbs.
 
-Note: I would have named 
+Note: I would have named "utils" 
 """
 
 _db_instance = None
