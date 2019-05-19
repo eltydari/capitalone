18ebc47824041c8eb92a11b553df4fb9f4641ec8
HackerRank <hackerrank@example.com>
2019-05-19 06:49:43 +0000

    Saved at Sun May 19 2019 02:49:44 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -1,6 +1,6 @@
 from werkzeug.exceptions import abort
 from weathertracker.measurement_store import query_measurements
-from collections 
+from collections import Or
 
 # Registered metrics will use metadata generated within get_stats
 STATS = {
