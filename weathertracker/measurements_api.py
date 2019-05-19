ea7975b24dea6f8bb0963c41d7bfc7bd915d377f
HackerRank <hackerrank@example.com>
2019-05-19 04:00:28 +0000

    Saved at Sun May 19 2019 00:00:29 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -1,7 +1,6 @@
 from   flask import request, jsonify
 from   flask.views import MethodView
 from   werkzeug.wrappers import Response
-from   werkzeug.datastructures import HEd
 from   werkzeug.exceptions import abort, BadRequest
 import weathertracker.measurement_store as mdb
 from   weathertracker.utils.conversion import (
