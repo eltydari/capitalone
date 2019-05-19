e27a40e937c646cfb43f18b4166a7bf1f13a145e
HackerRank <hackerrank@example.com>
2019-05-19 07:01:03 +0000

    Saved at Sun May 19 2019 03:01:04 GMT-0400 (Eastern Daylight Time)

--- weathertracker/stats.py
+++ weathertracker/stats.py
@@ -52,7 +52,7 @@ class Stats(object):
                     metadata["max"] = metric
                 elif metric > metadata["max"]:
                     metadata["max"] = metric
-        if validate_metadata(meta)
+        if validate_metadata(metadata):
         self._metadata[metric_name] = metadata
         
     def _aggregate_metrics(self, metrics):
