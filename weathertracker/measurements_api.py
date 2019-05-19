b3836f2450deb9fe55a2dbb3ee804f223c1b0e64
HackerRank <hackerrank@example.com>
2019-05-19 05:00:37 +0000

    Saved at Sun May 19 2019 01:00:38 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -40,6 +40,7 @@ class MeasurementsAPI(MethodView):
         timestamp = req.get("timestamp")
         if not timestamp:
             raise BadRequest(description = "Input timestamp was expected but not provided.")
+
         
         process_measurement(req)
         mdb.add_measurement(req)
