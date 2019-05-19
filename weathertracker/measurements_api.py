698f6e61c6a4229ea8ba69981dfbfa9c0593ed87
HackerRank <hackerrank@example.com>
2019-05-19 05:09:07 +0000

    Saved at Sun May 19 2019 01:09:08 GMT-0400 (Eastern Daylight Time)

--- weathertracker/measurements_api.py
+++ weathertracker/measurements_api.py
@@ -30,7 +30,7 @@ def process_measurement(req_json):
     return req_json
     
     
-def stringify():
+def stringify(dict_itm):
     # Main purpose is to turn '0.0' into '0'
     for key, value in ret_json.items():
         if value == 0:
